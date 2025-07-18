282d36637fa71da82999f74c2cd81191685534c3
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-18 15:31:48 -0700

    organized directions photos

--- src/components/Directions/Directions.jsx
+++ src/components/Directions/Directions.jsx
@@ -7,20 +7,30 @@ import or from "../../images/oregon-logo.png";
 function Directions() {
   return (
     <div className="directions">
-      <p className="directions__title">New York</p>
-      <img className="directions__img" src={ny} alt="new-york" />
+      <div className="directions__wrapper-1">
+        <p className="directions__title">New York</p>
+        <img className="directions__img" src={ny} alt="new-york" />
+      </div>
       <div className="directions__line"></div>
-      <p className="directions__title">Tenessee</p>
-      <img className="directions__img" src={tn} alt="tennessee" />
+      <div className="directions__wrapper-2">
+        <p className="directions__title">Tenessee</p>
+        <img className="directions__img" src={tn} alt="tennessee" />
+      </div>
       <div className="directions__line"></div>
-      <p className="directions__title">Georgia</p>
-      <img className="directions__img" src={ga} alt="georgia" />
+      <div className="directions__wrapper-1">
+        <p className="directions__title">Georgia</p>
+        <img className="directions__img" src={ga} alt="georgia" />
+      </div>
       <div className="directions__line"></div>
-      <p className="directions__title">Arizona</p>
-      <img className="directions__img" src={ar} alt="arizona" />
+      <div className="directions__wrapper-2">
+        <p className="directions__title">Arizona</p>
+        <img className="directions__img" src={ar} alt="arizona" />
+      </div>
       <div className="directions__line"></div>
-      <p className="directions__title">Oregon</p>
-      <img className="directions__img" src={or} alt="oregon" />
+      <div className="directions__wrapper-1">
+        <p className="directions__title">Oregon</p>
+        <img className="directions__img" src={or} alt="oregon" />
+      </div>
     </div>
   );
 }

5281ba2c416c76354509e1fc1fae7a4ceec82a09
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-20 16:50:43 -0700

    added api calls for python preperation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,56 @@
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Main from "./Main/Main";
 
+import * as api from "../utils/api";
+
 function App() {
+  const [places, setPlaces] = useState([]);
+  const [route, setRoute] = useState(null);
+  const [singlePlace, setSinglePlace] = useState(null);
+
+  useEffect(() => {
+    // get places
+    api
+      .getPlaces()
+      .then((data) => {
+        setPlaces(data);
+      })
+      .catch(console.error);
+
+    // get routes
+    api
+      .getRoute([
+        "New York",
+        "Orlando",
+        "St. Louis",
+        "Mt. Rushmore",
+        "San Francisco",
+      ])
+      .then((route) => {
+        setRoute(route);
+      })
+      .catch(console.error);
+
+    // get a single place by Id
+    api
+      .getPlaceById(1)
+      .then((place) => {
+        setSinglePlace(place);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <Main places={places} route={route} singlePlace={singlePlace} />
+          }
+        />
       </Routes>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,37 +4,47 @@ import stlouis from "../images/st-louis/st-louis-logo.jpeg";
 import mtrush from "../images/mt-rush/mt-rush-logo.jpg";
 import sanfran from "../images/san-fran/san-fran-logo.avif";
 
-const places = [
+const cities = [
   {
     name: "New York",
     route: "/new-york",
     image: newyork,
     className: "map__ny-logo",
+    latitude: "",
+    longitude: "",
   },
   {
     name: "Orlando",
     route: "/orlando",
     image: orlando,
     className: "map__orlando-logo",
+    latitude: "",
+    longitude: "",
   },
   {
     name: "St. Louis",
     route: "/st-louis",
     image: stlouis,
     className: "map__st-logo",
+    latitude: "",
+    longitude: "",
   },
   {
     name: "Mt. Rushmore",
     route: "/mt-rushmore",
     image: mtrush,
     className: "map__mt-logo",
+    latitude: "",
+    longitude: "",
   },
   {
     name: "San Fransisco",
     route: "/san-fransisco",
     image: sanfran,
     className: "map__sanfran-logo",
+    latitude: "",
+    longitude: "",
   },
 ];
 
-export { places };
+export { cities };

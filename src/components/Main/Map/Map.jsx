052ae1898ddafba6648ddf19dd969fec445d57c1
Liam Devine <skynotebook@proton.me>
2025-07-22 00:05:18 -0400

    title change

--- src/components/Main/Map/Map.jsx
+++ src/components/Main/Map/Map.jsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { places } from "../../../utils/constants";
+// import newyork from "../images/new-york/new-york-logo.png";
+// import orlando from "../images/orlando/orlando-logo.jpg";
+// import stlouis from "../images/st-louis/st-louis-logo.jpeg";
+// import mtrush from "../images/mt-rush/mt-rush-logo.jpg";
+// import sanfran from "../images/san-fran/san-fran-logo.avif";
 
 function Map() {
   const [typedInfo, setTypedInfo] = useState("");
@@ -16,10 +21,18 @@ function Map() {
     );
   }, [typedInfo]);
 
+  // // Create the icon object
+  // const customIcon = L.icon({
+  //   iconUrl: place.image,
+  //   iconSize: [40, 40], // adjust size as needed
+  //   iconAnchor: [20, 40], // anchor (bottom-center of icon)
+  //   popupAnchor: [0, -40], // popup appears above marker
+  // });
+
   return (
     // Map imputs
     <div className="map">
-      <h1 className="map__title">East to West</h1>
+      <h1 className="map__title">SunChaser</h1>
       <div className="map__userInput">
         <div className="map__searchBar">
           <input
@@ -44,6 +57,7 @@ function Map() {
       <MapContainer
         center={[39.5, -98.35]}
         zoom={4}
+        // placeholder={"interactive map"} Placeholder doesn't work
         style={{ height: "500px", width: "100%" }} // leaflet css div's don't work with markers
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
@@ -52,6 +66,8 @@ function Map() {
           <Marker
             key={place.landmark}
             position={[Number(place.latitude), Number(place.longitude)]}
+            alt={`${place.landmark} image`}
+            // iconUrl={customIcon}
           >
             <Popup>
               <strong>{place.landmark}</strong>
